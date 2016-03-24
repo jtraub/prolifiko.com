61a3572c3f44b4385b67f2c1b97776f0c109e66c
Mike Fisher <mike@mbfisher.com>
2016-03-24 12:22:33 +0000

    Fix requirements for Heroku

--- prolifiko/settings/production.py
+++ prolifiko/settings/production.py
@@ -1,7 +1,14 @@
+import dj_database_url
+
+
 from .base import *
 
 
 DEBUG = False
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 try:
     from .local import *
