cdc119ac0bcbd2f3f9369ddff13e92fc13cc2c65
Mike Fisher <mike@mbfisher.com>
2016-03-24 13:07:30 +0000

    Use production settings

--- prolifiko/settings/production.py
+++ prolifiko/settings/production.py
@@ -5,6 +5,7 @@ from .base import *
 
 
 DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
