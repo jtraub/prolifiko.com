0bc002f1bb1089a98ff1ec2173ba22d00e1ed4a7
Mike Fisher <mike@mbfisher.com>
2016-03-24 13:21:13 +0000

    Add deployment settings

--- prolifiko/settings/production.py
+++ prolifiko/settings/production.py
@@ -6,6 +6,14 @@ from .base import *
 
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS = ['prolifikoapp.herokuapp.com']
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+
+CSRF_COOKE_HTTPONLY = True
+
+X_FRAME_OPTIONS = 'DENY'
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
