e2ed3b51b71c7138c01a4c4aef099a9dc684c768
Mike Fisher <mike@mbfisher.com>
2016-03-24 13:15:15 +0000

    Reset wsgi settings

--- prolifiko/wsgi.py
+++ prolifiko/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "prolifiko.settings.production"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prolifiko.settings")
 
 application = get_wsgi_application()
