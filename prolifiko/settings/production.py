970387fda0c64c9230150e45234296971fb8b58b
Mike Fisher <mike@mbfisher.com>
2016-09-26 19:47:38 +0100

    Redirect http to https

--- prolifiko/settings/production.py
+++ prolifiko/settings/production.py
@@ -13,6 +13,10 @@ ALLOWED_HOSTS = [urlparse(BASE_URL).hostname]
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
