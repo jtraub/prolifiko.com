fb6ff13f598e046d9c3c56467de3b777e88949be
Daniel Jones <dan@dans-retina-mbp.local>
2016-10-23 21:17:51 +0100

    dashboard now hides completed data which is revealed on clicks

--- app/static/index.js
+++ app/static/index.js
@@ -1,9 +1,10 @@
 require('./index.scss');
+var $ = require('jquery');
 
 var LimitTextDone = [];
 var MAX_WORDS = 100;
 
-window.limitText = function limitText(textarea, requireText=true) {
+window.limitText = function limitText(textarea, requireText = true) {
     if (LimitTextDone.indexOf(textarea) > -1) {
         return;
     }
@@ -69,3 +70,16 @@ if (timezoneInput) {
     console.log('Guessing timezone as', guess);
     timezoneInput.value = guess;
 }
+
+$(document).ready(function() {
+
+    $('.goal-name').on('click', function () {
+        $(this).siblings('.steps').toggle();
+    });
+
+    $('.step').on('click', function () {
+        $(this).children('.step-text').toggle();
+    });
+
+});
+
