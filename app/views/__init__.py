9ed2707c26d37207f83fb836febb049c5b78885f
Mike Fisher <mike@mbfisher.com>
2016-11-12 08:29:22 +0000

    Start to refactor in to separate pages

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -6,14 +6,16 @@ from django.shortcuts import redirect, render
 
 @login_required
 def index(request):
-    active_goals = Goal.objects.filter(user=request.user, complete=False)
+    active_goal = Goal.objects \
+        .filter(user=request.user, complete=False) \
+        .first()
     completed_goals = Goal.objects.filter(user=request.user, complete=True)
 
-    if len(active_goals) == 0 and len(completed_goals) == 0:
+    if not active_goal and len(completed_goals) == 0:
         return redirect('new_goal')
 
     return render(request, 'myprogress.html', {
-        'active_goals': active_goals,
+        'active_goal': active_goal,
         'completed_goals': completed_goals,
         'is_subscribed': is_user_subscribed(request.user)
     })
